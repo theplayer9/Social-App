91d9b9b62257479c51a96f33206b376f62e7f453
Om Sharma <omsharma.theplayer11@gmail.com>
2022-04-21 13:09:32 +0530

    Made the basic structure.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import Navbar from  './components/Navbar'
+import Rightbar from './components/Rightbar'
+import Sidebar from './components/Sidebar'
+import Feed from './components/Feed'
+
 
 function App() {
   return (
     <div >
       <p> theplayer_9</p>
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+      <Navbar />
     </div>
   );
 }
