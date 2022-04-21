5e9c0c43bd6d9200bd8fe7477bd5335318bb1c3e
Om Sharma <omsharma.theplayer11@gmail.com>
2022-04-21 17:42:34 +0530

    implemented the menu feature on profile pic(Avatar)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,86 @@
 import styled from "@emotion/styled";
-import { AppBar, Toolbar, Typography } from "@mui/material";
-import React from "react";
-import FacebookIcon from '@mui/icons-material/Facebook';
-
-
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  InputBase,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import { Box, flexbox } from "@mui/system";
+import { Mail, Notifications } from "@mui/icons-material";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
 });
 
-const Search = styled("div")(({theme}) =>({
+//theme is not working here
+const Search = styled("div")(({ theme }) => ({
   backgroundColor: "white",
-}))
+  padding: "0 10px",
+  borderRadius: "5px",
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  // backgroundColor: "white",
+  display: "flex",
+  gap: "20px",
+  alignItems: "center",
+}));
+
+// const UserBox = styled(Box)(({ theme }) => ({
+//   // backgroundColor: "white",
+//   display: "flex",
+//   gap: "20px",
+//   alignItems: "center",
+// }));
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
   return (
     <AppBar position="sticky">
       <StyledToolbar>
-        <Typography variant="h6" sx={{display:{ xs: "none", sm: "block"}}} >Facebook</Typography>
-        <FacebookIcon sx={{display:{ xs: "block", sm: "none"}}} />
-        <Search> Search</Search>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Facebook
+        </Typography>
+        <FacebookIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={3} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar onClick={(e)=>(setOpen(true))} sx={{ bgcolor: "pink" }}>OP</Avatar>
+        </Icons>
       </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={e=>setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <MenuItem >Profile</MenuItem>
+        <MenuItem >My account</MenuItem>
+        <MenuItem >Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
