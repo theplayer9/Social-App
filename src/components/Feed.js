d2698052b5b3334d4bdfb771413c62f1f6dcceff
Om Sharma <omsharma.theplayer11@gmail.com>
2022-04-21 18:30:40 +0530

    added a component in the feed section

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,10 +1,49 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Favorite, MoreVert, Share } from "@mui/icons-material";
+import { Avatar, Box, Card, CardActions, CardContent, CardHeader, CardMedia, IconButton, Typography } from "@mui/material";
+import React from "react";
 
 const Feed = () => {
   return (
-    <Box bgcolor="red" flex={4} padding={2} > Feed</Box>
-  )
-}
+    <Box  flex={4} padding={2}>
+      <Card >
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVert />
+            </IconButton>
+          }
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image="/img.jpg"
+          alt="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+          <Favorite />
+          </IconButton>
+          <IconButton aria-label="share">
+            <Share />
+          </IconButton>
+          </CardActions>
+      </Card>
+    </Box>
+  );
+};
 
-export default Feed
+export default Feed;
